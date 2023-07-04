fb5516d63a84002850b554c1a90cb8a02981e12e
Sarun Daunghirun <125266560+sdsarun@users.noreply.github.com>
2023-07-04 21:17:58 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 export default function Footer({ backgroundColor, leftTextColor, rightTextColor, borderColor, isHideBorder }) {
+  const leftText = "JD";
+  const rightText = "© 2023 by John Doe";
   return (
     <footer className="px-10 py-4 bg-black text-white ">
       <div className="flex justify-between items-center border-t-2 border-gray-400">
         <div>
-          <h4 className="font-bold">WK</h4>
+          <h4 className="font-bold">{leftText}</h4>
         </div>
         <div className="py-4">
-          <h4 className="">© 2023 by Wuttichai Kantaseema</h4>
+          <h4 className="">{rightText}</h4>
         </div>
       </div>
     </footer>
